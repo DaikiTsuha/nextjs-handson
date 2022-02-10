ebd7aebc29a38664ed499d05f9cefd0cbcbadf7f
DaikiTsuha <tsuha_daiki@ca-adv.co.jp>
2022-02-10 17:49:11 +0900

    new: 実際に店を検索する画面を作成。検索する店を沖縄に修正

--- pages/api/shops.js
+++ pages/api/shops.js
@@ -6,7 +6,7 @@ const shops = async (req, res) => {
   const query = new URLSearchParams();
   query.set('key', HOTPEPPER_API_KEY);
   query.set('format', 'json');
-  query.set('large_area', req.query.large_area || 'Z011');
+  query.set('large_area', req.query.large_area || 'Z098');
   if (req.query.keyword) query.set('keyword', req.query.keyword);
 
   const response = await fetch(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?${query.toString()}`);
